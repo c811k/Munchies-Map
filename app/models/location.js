2de125190af87a214480df0947a8bdb979629199
c811k <bhk811@gmail.com>
2019-01-19 21:46:56 -0800

    location model

--- app/models/location.js
+++ app/models/location.js
@@ -1,6 +1,23 @@
 module.exports = function(sequelize, DataTypes) {
     var Location = sequelize.define("Location", {
-       
+       latitude: {
+           type: DataTypes.DECIMAL(10, 5),
+           allowNull: false
+       },
+       longitude: {
+           type: DataTypes.DECIMAL(10, 5),
+           allowNull: false
+       }
     });
+    
+    Location.associate = function(models) {
+        this.belongTo(models.Vendor, {
+            foreignKey: {
+                allowNull: false,
+                name: "vendorId"
+            }
+        });
+    }
+
     return Location;
 }
--- app/public/js/profile.js
+++ app/public/js/profile.js
@@ -12,6 +12,25 @@ $(document).ready(function() {
         submitItem(newItem);
     });
 
+    $("#status").on("click", function () {
+        if (navigator.geolocation) {
+
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                    position: google.maps.ControlPosition.TOP_CENTER
+                };
+
+                console.log(pos);
+                // infoWindow.setPosition(pos);
+                // infoWindow.setContent('<h1>You are here</h1>');
+                // infoWindow.open(map);
+                // map.setCenter(pos);
+            });
+        }
+    });
+
     function submitItem(nItem) {
         $.post("/api/menu", nItem, function() {
             alert("Good Luck!");
