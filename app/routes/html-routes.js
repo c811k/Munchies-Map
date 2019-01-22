8c94582a7f4c6b24e4fd9707ed99619d0099219f
Ahmed89TN <ahmedbenothmen045@gmail.com>
2019-01-22 11:48:30 -0800

    error page

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -22,6 +22,8 @@ module.exports = function(app) {
   app.get("/profile", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/profile.html"));
   });
-
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/error.html"));
+  });
 };
 
