8b84b422fb8fd183e1fca7e7bbe5a3af73b4e430
irwing gameros <irwinggameros@irwings-MacBook-Pro.local>
2019-01-22 14:53:45 -0800

    fixed info boxes

--- app/public/js/location.js
+++ app/public/js/location.js
@@ -67,17 +67,50 @@ function initMap() {
     // marker = new google.maps.Marker({ position: uluru, map: map });
     // var marker2 = new google.maps.Marker({ position: uluru2, map: map });
 
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map,
-        title: 'Hello World!'
-    });
+    var contentString = '<div id="content">' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+        '<div id="bodyContent">' +
+        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+        'sandstone rock formation in the southern part of the ' +
+        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+        'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+        'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+        'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+        'Aboriginal people of the area. It has many springs, waterholes, ' +
+        'rock caves and ancient paintings. Uluru is listed as a World ' +
+        'Heritage Site.</p>' +
+        '</div>' +
+        '</div>';
 
-    var marker2 = new google.maps.Marker({
-        position: uluru2,
-        map: map,
-        title: 'Hello World!'
-    });
+        var infowindow2 = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 200
+          });
+
+          var marker = new google.maps.Marker({
+            position: uluru,
+            map: map,
+            title: 'Uluru (Ayers Rock)'
+          });
+
+          marker.addListener('click', function() {
+            infowindow2.open(map, marker);
+          });
+
+    // var marker = new google.maps.Marker({
+    //     position: uluru,
+    //     map: map,
+    //     title: 'Hello World!'
+    // });
+
+    // var marker2 = new google.maps.Marker({
+    //     position: uluru2,
+    //     map: map,
+    //     title: 'Hello World!'
+    // });
 
     var markers = [];
     var infoWindows = [];
